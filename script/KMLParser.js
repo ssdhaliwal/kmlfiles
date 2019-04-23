4bbda48e5801d4fbd6c857501dda300cd1a7fb41
Seraj Dhaliwal <serajdhaliwal@gmail.com>
2019-04-23 13:39:18 -0400

    updated style processing
    - still in work - finalizing styles

--- script/KMLParser.js
+++ script/KMLParser.js
@@ -18,10 +18,9 @@ class KMLParser {
                 "colorMode": "random",              /* normal, random */
                 "scale": "1.1",
                 "Icon": {
-                    "href": "https://maps.google.com/mapfiles/kml/pal3/icon21.png",
+                    "href": "https://google.com/mapfiles/ms/micons/green-dot.png",
                 },
-                "heading": "0.0",
-                "hotSpot": ""
+                "heading": "0.0"
             },
             "LabelStyle": {
                 "color": "ff0000cc",
@@ -43,8 +42,14 @@ class KMLParser {
                 "listItemType": "checkHideChildren",    /* radioFolder, check, checkHideChildren, checkOffOnly */
                 "bgColor": "ff336699",
                 "ItemIcon": {
-                    "state": "closed",
-                    "href": "https://maps.google.com/mapfiles/kml/pal3/icon21.png",
+                    0: {
+                        "state": "closed",
+                        "href": "https://maps.google.com/mapfiles/kml/pal5/icon3.png",
+                    },
+                    1: {
+                        "state": "open",
+                        "href": "https://maps.google.com/mapfiles/kml/pal5/icon11.png",
+                    }
                 },
                 "maxSnippetLines": "5"
             },
@@ -54,10 +59,6 @@ class KMLParser {
                 "displayMode": "default"                /* default, hide */
             }
         };
-    };
-
-    _defaultStyles(styleType) {
-
     };
 
     processKml(kmlString) {
@@ -138,13 +139,13 @@ class KMLParser {
                     skipChildren = true;
 
                     let id = this.getNodeAttribute(childItem, "id");
-                    this.styles[id] = childItem;
+                    this.styles["#" + id] = childItem;
                 }
                 if (childItem.nodeName === "StyleMap") {
                     skipChildren = true;
 
                     let id = this.getNodeAttribute(childItem, "id");
-                    this.styleMaps[id] = childItem;
+                    this.styleMaps["#" + id] = childItem;
                 }
 
                 // scan child nodes
@@ -159,26 +160,14 @@ class KMLParser {
         return folders;
     };
 
-    getNodeBooleanValue(node) {
-        var value = this.getNodeTextValue(node);
-
+    getBoolean(value) {
         switch (value.toLowerCase()) {
-            case "error": case "reject": case "closed": case "hide": 
+            case "error": case "reject": case "closed": case "hide":
             case "false": case "off": case "no":
             case "0":
             case null: case undefined: return false;
             default: return true;
         }
-    };
-
-    getNodeIntegerValue(node) {
-        var value = this.getNodeTextValue(node);
-        return parseInt(value, 10);
-    };
-
-    getNodeFloatValue(node) {
-        var value = this.getNodeTextValue(node);
-        return parseFloat(value);
     };
 
     getNodeValue(node, element) {
@@ -199,29 +188,276 @@ class KMLParser {
         return value;
     };
 
-    createLayer() {
+    getRGBColor(color) {
+        let value = {}, bb, gg, rr;
 
+        color = color.replace("#", "");
+
+        if (color) {
+            if (color.length === 8) {
+                bb = color.substr(2, 2);
+                gg = color.substr(4, 2);
+                rr = color.substr(6, 2);
+
+                value = "#" + rr + gg + bb;
+            } else if (color.length === 6) {
+                bb = color.substr(0, 2);
+                gg = color.substr(2, 2);
+                rr = color.substr(4, 2);
+
+                value = "#" + rr + gg + bb;
+            } else {
+                value = getRandomColor("ffffff");
+            }
+        } else {
+            value = getRandomColor("ffffff");
+        }
+
+        return value;
     };
 
-    buildStyle(styles) {
-        // IconStyle
-        // LineStyle
-        // LineStyle
-        // PolyStyle
-        // BalloonStyle
-        // ListStyle
-        // LabelStyle
+    getRGBOpacity(color) {
+        let value = {}, aa;
 
-        // parse all styles and store style info
-        console.log(styleNodes);
-        var styleId, styleNode, nodeId;
-        Object.keys(styleNodes).forEach(function (style) {
-            nodeId = style.getAttribute("id");
+        color = color.replace("#", "");
 
-            if (nodeId) {
-                styleId = "#" + nodeId;
-                console.log(styleId);
+        if (color) {
+            if (color.length === 8) {
+                aa = color.substr(0, 2);
+
+                value = parseFloat(parseInt(aa, 16) / 256).toFixed(1);
+            } else {
+                value = 1.0;
             }
+        } else {
+            value = 1.0;
+        }
+
+        return value;
+    };
+
+    getRandomColor(color) {
+        let aa = "ff";
+        let bb = "ff";
+        let gg = "ff";
+        let rr = "ff";
+
+        color = color.replace("#", "");
+
+        if (color) {
+            if (color.length === 8) {
+                aa = color.substr(0, 2);
+                bb = color.substr(2, 2);
+                gg = color.substr(4, 2);
+                rr = color.substr(6, 2);
+            } else if (color.length === 6) {
+                bb = color.substr(0, 2);
+                gg = color.substr(2, 2);
+                rr = color.substr(4, 2);
+            }
+        }
+
+        if (bb !== "00") {
+            bb = Math.round(parseInt(bb, 16) * Math.random()).toString(16);
+        }
+        if (gg !== "00") {
+            gg = Math.round(parseInt(gg, 16) * Math.random()).toString(16);
+        }
+        if (rr !== "00") {
+            rr = Math.round(parseInt(rr, 16) * Math.random()).toString(16);
+        }
+
+        return "#" + aa + bb + gg + rr;
+    };
+
+    getDefaultStyle(styleType) {
+        return this.stylesDefault[styleType];
+    };
+
+    getStyle(styleUrl) {
+        let value = {};
+        let styleNode = this.styles[styleUrl];
+
+        if (styleNode) {
+            console.log(styleNode);
+
+            // IconStyle
+            let childNodes = styleNode.getElementsByTagName("IconStyle")[0];
+            if (childNodes) {
+                value.IconStyle = this.getDefaultStyle("IconStyle");
+                let color = this.getNodeValue(childNodes, "color");
+                if (color) {
+                    value.IconStyle.color = color;
+                }
+
+                let colorMode = this.getNodeValue(childNodes, "colorMode");
+                if (!colorMode) {
+                    colorMode = value.IconStyle.colorMode;
+                }
+                if (colorMode === "random") {
+                    value.IconStyle.color = this.getRandomColor(value.IconStyle.color);
+                }
+
+                let scale = this.getNodeValue(childNodes, "scale");
+                if (scale) {
+                    value.IconStyle.scale = scale;
+                }
+
+                let heading = this.getNodeValue(childNodes, "heading");
+                if (heading) {
+                    value.IconStyle.heading = heading;
+                }
+
+                let iconNodes = childNodes.getElementsByTagName("Icon")[0];
+                if (iconNodes) {
+                    value.IconStyle.Icon.href = this.getNodeValue(iconNodes, "href");
+                }
+            }
+
+            // LineStyle
+            childNodes = styleNode.getElementsByTagName("LineStyle")[0];
+            if (childNodes) {
+                value.LineStyle = this.getDefaultStyle("LineStyle");
+                let color = this.getNodeValue(childNodes, "color");
+                if (color) {
+                    value.LineStyle.color = color;
+                }
+
+                let colorMode = this.getNodeValue(childNodes, "colorMode");
+                if (!colorMode) {
+                    colorMode = value.LineStyle.colorMode;
+                }
+                if (colorMode === "random") {
+                    value.LineStyle.color = this.getRandomColor(value.LineStyle.color);
+                }
+
+                let width = this.getNodeValue(childNodes, "width");
+                if (width) {
+                    value.LineStyle.width = width;
+                }
+            }
+
+            // PolyStyle
+            childNodes = styleNode.getElementsByTagName("PolyStyle")[0];
+            if (childNodes) {
+                value.PolyStyle = this.getDefaultStyle("PolyStyle");
+                let color = this.getNodeValue(childNodes, "color");
+                if (color) {
+                    value.PolyStyle.color = color;
+                }
+
+                let colorMode = this.getNodeValue(childNodes, "colorMode");
+                if (!colorMode) {
+                    colorMode = value.PolyStyle.colorMode;
+                }
+                if (colorMode === "random") {
+                    value.PolyStyle.color = this.getRandomColor(value.PolyStyle.color);
+                }
+
+                let fill = this.getNodeValue(childNodes, "fill");
+                if (fill) {
+                    value.PolyStyle.fill = fill;
+                }
+
+                let outline = this.getNodeValue(childNodes, "outline");
+                if (outline) {
+                    value.PolyStyle.outline = outline;
+                }
+            }
+
+            // BalloonStyle
+            childNodes = styleNode.getElementsByTagName("BalloonStyle")[0];
+            if (childNodes) {
+                value.BalloonStyle = this.getDefaultStyle("BalloonStyle");
+                let bgColor = this.getNodeValue(childNodes, "bgColor");
+                if (bgColor) {
+                    value.BalloonStyle.bgColor = bgColor;
+                }
+
+                let colorMode = this.getNodeValue(childNodes, "colorMode");
+                if (!colorMode) {
+                    colorMode = value.BalloonStyle.colorMode;
+                }
+                if (colorMode === "random") {
+                    value.BalloonStyle.bgColor = this.getRandomColor(value.BalloonStyle.bgColor);
+                }
+
+                let textColor = this.getNodeValue(childNodes, "textColor");
+                if (textColor) {
+                    value.BalloonStyle.textColor = textColor;
+                }
+
+                let displayMode = this.getNodeValue(childNodes, "displayMode");
+                if (displayMode) {
+                    value.BalloonStyle.displayMode = displayMode;
+                }
+            }
+
+            // ListStyle
+            childNodes = styleNode.getElementsByTagName("ListStyle")[0];
+            if (childNodes) {
+                value.ListStyle = this.getDefaultStyle("ListStyle");
+                let bgColor = this.getNodeValue(childNodes, "bgColor");
+                if (bgColor) {
+                    value.ListStyle.bgColor = bgColor;
+                }
+
+                let colorMode = this.getNodeValue(childNodes, "colorMode");
+                if (!colorMode) {
+                    colorMode = value.ListStyle.colorMode;
+                }
+                if (colorMode === "random") {
+                    value.ListStyle.bgColor = this.getRandomColor(value.ListStyle.bgColor);
+                }
+
+                let listItemType = this.getNodeValue(childNodes, "listItemType");
+                if (listItemType) {
+                    value.ListStyle.listItemType = listItemType;
+                }
+
+                let maxSnippetLines = this.getNodeValue(childNodes, "maxSnippetLines");
+                if (maxSnippetLines) {
+                    value.ListStyle.maxSnippetLines = maxSnippetLines;
+                }
+
+                let itemIconNodes = childNodes.getElementsByTagName("ItemIcon");
+                if (itemIconNodes) {
+                    console.log(itemIconNodes);
+                }
+            }
+
+            // LabelStyle
+            childNodes = styleNode.getElementsByTagName("LabelStyle")[0];
+            if (childNodes) {
+                value.LabelStyle = this.getDefaultStyle("LabelStyle");
+                let color = this.getNodeValue(childNodes, "color");
+                if (color) {
+                    value.LabelStyle.color = color;
+                }
+
+                let colorMode = this.getNodeValue(childNodes, "colorMode");
+                if (!colorMode) {
+                    colorMode = value.LabelStyle.colorMode;
+                }
+                if (colorMode === "random") {
+                    value.LabelStyle.color = this.getRandomColor(value.LabelStyle.color);
+                }
+
+                let scale = this.getNodeValue(childNodes, "scale");
+                if (scale) {
+                    value.LabelStyle.scale = scale;
+                }
+            }
+        }
+
+        return value;
+    };
+
+    createLayer() {
+        let self = this;
+        Object.keys(self.styles).forEach(function (item) {
+            console.log(item, self.styles[item]); // value
+            console.log(self.getStyle(item));
         });
-    }
+    };
 };
